5a7af7bdd6da9b520d0b835b805cb88670a9dd50
3lviend <henryss_10@yahoo.com>
2018-11-22 14:06:48 +0700

    fix paginate

--- app/controllers/forum_threads_controller.rb
+++ app/controllers/forum_threads_controller.rb
@@ -9,7 +9,7 @@ class ForumThreadsController < ApplicationController
 			@threads = ForumThread.order(sticky_order: :asc).order(id: :desc)
 		end
 
-		@thread.paginate(per_page: 5, page: params[:id: :desc])
+		@threads = @thread.paginate(per_page: 5, page: params[:id: :desc])
 	end
 
 	def show
